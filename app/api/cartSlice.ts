100ec7a8304888993b2db85d0836e7e3a5f4370d
mdhasmat690 <mdhasmat690@gmail.com>
2023-01-12 16:31:34 +0600

    finish work

--- app/api/cartSlice.ts
+++ app/api/cartSlice.ts
@@ -1,18 +1,16 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit"
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { useAddToCartMutation } from "./apiSlice";
-import { Product } from "./productType"
+import { Product } from "./productType";
 
 export interface CartState {
-  productsInCart: Product[]
-  total: number
-  cartOpen: boolean
+  productsInCart: Product[];
+  total: number;
 }
 
 const initialState: CartState = {
   productsInCart: [],
   total: 0,
-  cartOpen: false,
-}
+};
 
 export const cartSlice = createSlice({
   name: "cart",
@@ -21,47 +19,28 @@ export const cartSlice = createSlice({
     addItemToCart: (state, action: PayloadAction<Product>) => {
       const productInCart = state.productsInCart.find(
         (product) => product.id === action.payload.id
-      )
+      );
       if (!productInCart) {
-        state.productsInCart.push({ ...action.payload, quantity: 1 })
-
-        state.total = state.productsInCart.reduce(
-          (acc, product) => acc + product.price * product.quantity,
-          0
-        )
+        state.productsInCart.push({ ...action.payload, quantity: 1 });
       } else {
-        productInCart.quantity++
-
-        state.total = state.productsInCart.reduce(
-          (acc, product) => acc + product.price * product.quantity,
-          0
-        )
+        productInCart.quantity++;
       }
-
     },
 
     deleteItemFromCart: (state, action: PayloadAction<Product>) => {
       const productInCartIndex = state.productsInCart.findIndex(
         (product) => product.id === action.payload.id
-      )
+      );
 
       if (state.productsInCart[productInCartIndex]?.quantity === 1) {
-        state.productsInCart.splice(productInCartIndex, 1)
-        state.total = state.productsInCart.reduce(
-          (acc, product) => acc + product.price * product.quantity,
-          0
-        )
+        state.productsInCart.splice(productInCartIndex, 1);
       } else {
-        state.productsInCart[productInCartIndex].quantity--
-        state.total = state.productsInCart.reduce(
-          (acc, product) => acc + product.price * product.quantity,
-          0
-        )
+        state.productsInCart[productInCartIndex].quantity--;
       }
-    }
+    },
   },
-})
+});
 
-export const { addItemToCart, deleteItemFromCart } = cartSlice.actions
+export const { addItemToCart, deleteItemFromCart } = cartSlice.actions;
 
-export default cartSlice.reducer
+export default cartSlice.reducer;
